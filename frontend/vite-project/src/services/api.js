de7f34360d323eba8cd1f06ffb88917d92a5ca95
saiteja-1-hub <A1@saiteja>
2026-06-22 22:33:43 +0530

    Use Render backend Url

--- frontend/vite-project/src/services/api.js
+++ frontend/vite-project/src/services/api.js
@@ -1,7 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({
-  baseURL: 'http://localhost:3000'
+export default axios.create({
+  baseURL: import.meta.env.VITE_API_URL
 });
-
-export default api;
