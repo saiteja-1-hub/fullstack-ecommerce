aceda9b5ab878fc1009d151bd61fd16b96b721c2
saiteja-1-hub <A1@saiteja>
2026-06-22 17:16:38 +0530

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 app.use('/products', productsRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
