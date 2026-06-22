5183d9d8c6197aac1e958aadb5634e63788006e8
saiteja-1-hub <A1@saiteja>
2026-06-22 17:27:51 +0530

    Perpare backend development

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,12 @@ app.use(express.json());
 
 app.use('/products', productsRoutes);
 
-const PORT = process.env.PORT || 3000
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
